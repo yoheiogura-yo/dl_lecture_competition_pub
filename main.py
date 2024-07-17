e9af1f47d02e98cce4866d6e3b1e6b1bf9086943
yoheiogura-yo <yohei.ogura.yo@gmail.com>
2024-07-17 21:52:01 +0900

    update source files and a config file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, sys
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torchvision import transforms
 from torchmetrics import Accuracy
 import hydra
 from omegaconf import DictConfig
@@ -13,6 +14,7 @@ from src.datasets import ThingsMEGDataset
 from src.models import BasicConvClassifier
 from src.utils import set_seed
 
+from matplotlib import pyplot as plt
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def run(args: DictConfig):
@@ -22,16 +24,21 @@ def run(args: DictConfig):
     if args.use_wandb:
         wandb.init(mode="online", dir=logdir, project="MEG-classification")
 
+
     # ------------------
     #    Dataloader
     # ------------------
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5)])
     loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
-    
     train_set = ThingsMEGDataset("train", args.data_dir)
+    train_set.transform = transform
     train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
     val_set = ThingsMEGDataset("val", args.data_dir)
     val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
     test_set = ThingsMEGDataset("test", args.data_dir)
+    test_set.transform = transform
     test_loader = torch.utils.data.DataLoader(
         test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
     )
@@ -46,7 +53,7 @@ def run(args: DictConfig):
     # ------------------
     #     Optimizer
     # ------------------
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.001)
 
     # ------------------
     #   Start training
@@ -56,6 +63,9 @@ def run(args: DictConfig):
         task="multiclass", num_classes=train_set.num_classes, top_k=10
     ).to(args.device)
       
+    visualization_train_loss, visualization_valid_loss = [], []
+    visualization_train_acc, visualization_valid_acc = [], []
+
     for epoch in range(args.epochs):
         print(f"Epoch {epoch+1}/{args.epochs}")
         
@@ -86,17 +96,61 @@ def run(args: DictConfig):
             
             val_loss.append(F.cross_entropy(y_pred, y).item())
             val_acc.append(accuracy(y_pred, y).item())
-
-        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}")
+        mean_train_loss = np.mean(train_loss)
+        mean_valid_loss =np.mean(val_loss)
+        mean_train_acc = np.mean(train_acc)
+        mean_valid_acc = np.mean(val_acc)
+        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {mean_train_loss:.3f} | train acc: {mean_train_acc:.3f} | val loss: {mean_valid_loss:.3f} | val acc: {mean_valid_acc:.3f}")
         torch.save(model.state_dict(), os.path.join(logdir, "model_last.pt"))
         if args.use_wandb:
-            wandb.log({"train_loss": np.mean(train_loss), "train_acc": np.mean(train_acc), "val_loss": np.mean(val_loss), "val_acc": np.mean(val_acc)})
+            wandb.log({"train_loss": mean_train_loss, "train_acc": mean_train_acc, "val_loss": mean_valid_loss, "val_acc": mean_valid_acc})
         
+        visualization_train_loss.append(mean_train_loss)
+        visualization_valid_loss.append(mean_valid_loss)
+        visualization_train_acc.append(mean_train_acc)
+        visualization_valid_acc.append(mean_valid_acc)
+
         if np.mean(val_acc) > max_val_acc:
             cprint("New best.", "cyan")
             torch.save(model.state_dict(), os.path.join(logdir, "model_best.pt"))
             max_val_acc = np.mean(val_acc)
-            
+    
+    # ------------------
+    #   Visualization
+    # ------------------
+    graph_x = np.arange(1, args.epochs+1)
+    plt.figure()
+    plt.plot(graph_x, visualization_train_loss, label='train loss')
+    plt.plot(graph_x, visualization_valid_loss, label='valid loss')
+    plt.legend()
+    plt.ylabel('model error')
+    plt.xlabel('epochs')
+    plt.title('loss')
+    plt.savefig(os.path.join(logdir, "loss.png"))
+
+    plt.figure()
+    plt.plot(graph_x, visualization_train_acc, label='train acc')
+    plt.plot(graph_x, visualization_valid_acc, label='valid acc')
+    plt.legend()
+    plt.ylabel('acc')
+    plt.xlabel('epochs')
+    plt.title('loss')
+    plt.savefig(os.path.join(logdir, "acc.png"))
+
+    plt.figure()
+    _, ax = plt.subplots()
+    ax2 = ax.twinx()
+    ax.plot(graph_x, visualization_train_loss, label='train loss')
+    ax.plot(graph_x, visualization_valid_loss, label='valid loss')
+    ax2.plot(graph_x, visualization_train_acc, label='train acc', color='limegreen')
+    ax2.plot(graph_x, visualization_valid_acc, label='valid acc', color='black')
+    ax.legend()
+    ax2.legend()
+    ax.set_ylabel('loss')
+    ax.set_xlabel('epochs')
+    ax2.set_ylabel('acc')
+    ax.set_title('loss')
+    plt.savefig(os.path.join(logdir, "loss_and_acc.png"))
     
     # ----------------------------------
     #  Start evaluation with best model
